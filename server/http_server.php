990f2e1bdeeba67e4cec8c91bcf412df9c19ec8e
xxy2 <850886470@qq.com>
2018-04-05 12:42:34 +0800

    modify server

--- server/http_server.php
+++ server/http_server.php
@@ -11,10 +11,60 @@ $http->set(
     [
         'enable_static_handler' => true,
         'document_root' => "/var/www/html/swoole_imooc/public/static",
+        'worker_num'=>5
     ]
 );
-$http->on('request', function($request, $response) {
 
+$http->on('WorkerStart', function(swoole_server $server,$worker_id) {
+
+    define('APP_PATH', __DIR__ . '/../application/');
+
+    //加载框架文件
+    require __DIR__ . '/../thinkphp/base.php';
+});
+
+$http->on('request', function($request, $response) use ($http) {
+    if(isset($request->server)) {
+        foreach ($request->server as $k=>$v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    if(isset($request->header)) {
+        foreach ($request->header as $k=>$v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    if(isset($request->get)) {
+        foreach ($request->get as $k=>$v) {
+            $_GET[$k] = $v;
+        }
+    }
+
+    if(isset($request->post)) {
+        foreach ($request->post as $k=>$v) {
+            $_POST[$k] = $v;
+        }
+    }
+
+    ob_start();
+
+    try{
+        think\Container::get('app', [defined('APP_PATH') ? APP_PATH : ''])
+            ->run()
+            ->send();
+    }catch (Exception $e) {
+
+    }
+
+
+
+    $res = ob_get_contents();
+    ob_clean();
+
+    $response->end($res);
+    $http->close();
 });
 
 $http->start();
